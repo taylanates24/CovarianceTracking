23835f9dac113b4aa1a135a3f839dfe23e16be6e
taylanates <taylan.ates@data-boss.com.tr>
2021-11-16 13:18:14 +0300

    Add covariance_matrix function

--- covariance_tracker.py
+++ covariance_tracker.py
@@ -27,5 +27,30 @@ class CovarianceTracker:
 
         return lap_gray, sobelx_gray, sobely_gray
 
+    def covariance_matrix(self, lap_gray, sobelx_gray, sobely_gray, bbox):
 
+        lap_gray = lap_gray[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        sobelx_gray = sobelx_gray[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        sobely_gray = sobely_gray[bbox[1]:bbox[3], bbox[0]:bbox[2]]
 
+        y_len = bbox[3] - bbox[1]
+        x_len = bbox[2] - bbox[0]
+
+        x_coor_l = np.array(list(range(bbox[0], bbox[2])) * y_len).reshape(y_len, -1)
+        y_coor_l = np.array((np.array(list(range(bbox[1], bbox[3]))).reshape(-1, 1).tolist() * x_len)).reshape(-1,
+                                                                                                               y_len).transpose()
+
+        f = np.concatenate(
+            (y_coor_l[..., None], x_coor_l[..., None], lap_gray[..., None], sobelx_gray[..., None],
+             sobely_gray[..., None]),
+            axis=2).reshape(-1, 1, 5)
+
+        f_mean = np.array(
+            [f[:, :, 0].mean(), f[:, :, 1].mean(), f[:, :, 2].mean(), f[:, :, 3].mean(), f[:, :, 4].mean()])
+
+        asd = f - f_mean
+        total = np.dot(asd.transpose().squeeze(), asd.squeeze())
+
+        total = total / len(f)
+
+        return total
